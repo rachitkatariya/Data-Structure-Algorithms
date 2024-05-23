05563667cf37880ce198aa284eb862f589b16aae
rachitkatariyaa <raachitkumar2953@gmail.com>
2024-05-23 20:15:38 +0530

    add files

--- 02. Operators/practiseQues.cpp
+++ 02. Operators/practiseQues.cpp
@@ -14,9 +14,26 @@ int main()
 
     // Ques 2
 
-    int x = 10, y = 5;
-    int exp1 = (y * (x / y + x / y));
-    int exp2 = (y * x / y + y * x / y);
-    cout << exp1 << " ";
-    cout << exp2 << "\n";
+    // int x = 10, y = 5;
+    // int exp1 = (y * (x / y + x / y));
+    // int exp2 = (y * x / y + y * x / y);
+    // cout << exp1 << " ";
+    // cout << exp2 << "\n";
+
+
+    // Ques 3
+    
+    int x = 200, y = 50, z = 100;
+    if (x > y && y > z)
+    {
+        cout << "Hello \n";
+    }
+    if (z > y && z < x)
+    {
+        cout << "C++ \n";
+    }
+    if ((y + 200) < x && (y + 150) < z)
+    {
+        cout << "Hello C++ \n";
+    }
 }
