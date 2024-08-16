1f02d8d4ca91a69efb1e783f1a090314584195cf
rachitkatariyaa <raachitkumar2953@gmail.com>
2024-08-16 22:24:26 +0530

    add files

---
+++ 13. Vectors/practice.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<int> v;
+    for(int i=0; i<5; i++){
+        v.push_back(i);
+    }
+    cout<<v.size() <<endl;
+    cout<<v.capacity() << endl;
+}
